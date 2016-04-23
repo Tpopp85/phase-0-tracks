4f9f535b424485a6e36dd2778ad08f0a3946ed6a
Tommy Poppenhouse <tpoppenhouse@gmail.com>
2016-04-23 10:00:06 -0500

    Add method to do encoding

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,15 +1,34 @@
+def encode_string(letter)
+  vowels = "aeiou" 
+  if vowels.include?(letter)
+    vowels[vowels.index(letter)+1]
+  elsif letter == " "
+    letter
+  elsif vowels.include? (letter.downcase.next)
+    letter.next.next
+  else
+    letter.next
+  end
+end 
+
+
+
+
 puts "What is your name?"
+
 name = gets.chomp
 code_name = name.split(" ")
 code_name.reverse!
 p code_name
 code_name = code_name.join(" ")
 code_name = code_name.split('')
-vowels = "aeiou" 
 
-puts vowels.index("a")
 
-code_name.map! { |letter| ((vowels).include?(letter) ? vowels[vowels.index(letter)+1] : (letter == " " ? letter : (vowels.include?(letter.downcase.next) ? (letter.next).next : letter.next))) }
+code_name.map! { |letter| ((vowels).include?(letter) ? vowels[vowels.index(letter)+1] : 
+  (letter == " " ? letter : (vowels.include?(letter.downcase.next) ? 
+  (letter.next).next : letter.next))) }
+
 code_name = code_name.join("")
 
+puts name
 puts code_name
