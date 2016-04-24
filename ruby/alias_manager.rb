5c646ba235689ec3c625fce16e55090d6009b1b4
Tommy Poppenhouse <tpoppenhouse@gmail.com>
2016-04-24 11:26:02 -0500

    Add an algorithm and user interface to encode a name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,10 @@
 def encode_string(letter)
   vowels = "aeiou" 
-  if vowels.include?(letter)
+  if letter == "u"
+    "a"
+  elsif letter == "z"
+    "b"
+  elsif vowels.include?(letter)
     vowels[vowels.index(letter)+1]
   elsif letter == " "
     letter
@@ -11,34 +15,17 @@ def encode_string(letter)
   end
 end 
 
+code_name_array = []
 
-
-
-
- puts "What is your name?"
-
+puts "What is your name? (Type \"quit\" to escape)" 
 name = gets.chomp
-code_name = name.split(" ")
-code_name.reverse!
-p code_name
-code_name = code_name.join(" ")
-code_name = code_name.split('')
+while name != "quit"
+  code_name = name.split(" ").reverse!.join(" ").split('')
+  code_name.map! {|letter| encode_string(letter)}
+  code_name = code_name.join("")
+  code_name_array << {name: name, code_name: code_name}
+  name = gets.chomp
+end  
 
+code_name_array.each{|pair| puts "#{pair[:name]} is actually #{pair[:code_name]}"}
 
-code_name.map! do |letter| 
-  vowels = "aeiou" 
-  if vowels.include?(letter)
-    vowels[vowels.index(letter)+1]
-  elsif letter == " "
-    letter
-  elsif vowels.include? (letter.downcase.next)
-    letter.next.next
-  else
-    letter.next
-  end
- 
-end
-code_name = code_name.join("")
-
-puts name
-puts code_name
