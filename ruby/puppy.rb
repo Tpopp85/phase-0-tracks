c2d4fb4ca8bd1a75031260bc7cd60a7a39865fd2
Tommy Poppenhouse <tpoppenhouse@gmail.com>
2016-04-30 15:24:35 -0500

    Add puppy class framework

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+=begin
+Puppy Class
+
+Characteristics
+-Cuddliness factor: 1 to 10
+-Bark strength: yap to bark
+-name: varies
+-sleepiness: varies
+
+Behavior
+-nap
+-whine
+-pee on carpet
+-play
+  
+end
